1e3e1ce7507baa59e3bc21f7709b3c3490b3d77d
Chenrt <2926777985@qq.com>
2022-07-29 19:12:41 +0800

    added linux support

--- core/runner.py
+++ core/runner.py
@@ -1,21 +1,26 @@
 import os
+import sys
+import platform
 import subprocess
 import webbrowser
 from urllib import parse
 
 name = 'run.exe'
-baseUrl = 'http://127.0.0.1:8080'
+localUrl = 'http://127.0.0.1:8080'
+remoteUrl = 'https://chenrt-ggx.github.io/MemoryMountain'
 
 if __name__ == '__main__':
     files = list(filter(lambda x: x.endswith('.c'), os.listdir()))
     cmd = ' '.join(['gcc'] + files + ['-o', name, '-O2'])
     print('--------------- compile ---------------\n' + cmd + '\n')
     os.system(cmd)
-    print('--------------- execute ---------------\n' + name + '\n')
-    runner = subprocess.Popen(name, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8")
+    cmd = name if 'windows' in platform.platform().lower() else './' + name
+    print('--------------- execute ---------------\n' + cmd + '\n')
+    runner = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8")
     out, err = runner.communicate()
     print('--------------- stdout ---------------\n' + out)
     print('--------------- stderr ---------------\n' + err)
+    baseUrl = localUrl if len(sys.argv) == 2 and sys.argv[1] == '-local' else remoteUrl
     url = baseUrl + '?map=' + parse.quote(out)
     print('--------------- request ---------------\n' + url)
     webbrowser.open(url)
