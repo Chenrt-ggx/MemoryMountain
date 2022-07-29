62e534c3ea459a6d78a9372a8967a950a73fbcfa
Chenrt <2926777985@qq.com>
2022-07-29 08:56:11 +0800

    config files

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true,
+    publicPath: process.env.VUE_APP_VERSION === 'BuildAction' ? './' : '/',
+    transpileDependencies: true,
+    productionSourceMap: false
 });
